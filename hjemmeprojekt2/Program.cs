b0fc63372c8a1faf947091ae176a7b486fd5703b
Alexander Diget <alex40@live.dk>
2023-09-27 10:34:16 +0200

    test push

--- hjemmeprojekt2/Program.cs
+++ hjemmeprojekt2/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("test push");
         }
     }
 }
+
+
+
+
+
